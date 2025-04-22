8016b53f8ce406968b72ed476681118660c31b56
Edu-1234 <dudu.miguel.karla.ganso@gmail.com>
2025-04-21 23:35:43 -0300

    Adicionado mais capitais

--- Class/Capital.java
+++ Class/Capital.java
@@ -4,9 +4,18 @@ public enum Capital {
     VITORIA("Vitória"),
     RIO_BRANCO("Rio Branco"),
     SALVADOR("Salvador"),
+    FORTALEZA("Fortaleza"),
+    BRASILIA("Brasília"),
     MACEIO("Maceió"),
     MANAUS("Manaus"),
-    BELO_HORIZONTE("Belo Horizonte");
+    BELO_HORIZONTE("Belo Horizonte"),
+    RIO_DE_JANEIRO("Rio de Janeiro"),
+    SAO_PAULO("São Paulo"),
+    ARACAJU("Aracaju"),
+    NATAL("Natal"),
+    RECIFE("Recife"),
+    GOIANIA("Goiânia"),
+    BELEM("Belém");
 
 
     private final String nome;
