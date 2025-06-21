5e38f9766c95e51365e3560c7acbfd483d484e8f
okanjota <jaimeejunioorr@gmail.com>
2025-06-21 02:23:26 -0300

    Capitais do Tocantins, Pernambuco, Minas Gerais, São Paulo, Alagoas, Sergipe, Paraíba, Piauí, Ceará, Maranhão, Rio Grande do Norte adcionados

--- src/Class/Capital.java
+++ src/Class/Capital.java
@@ -2,20 +2,20 @@ package Class;
 
 public enum Capital {
     BRASILIA("Brasília", "DF", 655, 541),
-    SAO_PAULO("São Paulo", "SP", 550, 600),
+    SAO_PAULO("São Paulo", "SP", 675, 740),
     RIO_DE_JANEIRO("Rio de Janeiro", "RJ", 756, 730),
-    BELO_HORIZONTE("Belo Horizonte", "MG", 570, 540),
+    BELO_HORIZONTE("Belo Horizonte", "MG", 745, 650),
     SALVADOR("Salvador", "BA", 883, 481),
-    MACEIO("Maceió", "AL", 730, 420),
-    RECIFE("Recife", "PE", 760, 400),
-    JOAO_PESSOA("João Pessoa", "PB", 770, 380),
-    NATAL("Natal", "RN", 780, 360),
-    FORTALEZA("Fortaleza", "CE", 750, 320),
-    SAO_LUIS("São Luís", "MA", 740, 234),
+    MACEIO("Maceió", "AL", 955, 400),
+    RECIFE("Recife", "PE", 980, 365),
+    JOAO_PESSOA("João Pessoa", "PB", 981, 340),
+    NATAL("Natal", "RN", 974, 310),
+    FORTALEZA("Fortaleza", "CE", 888, 245),
+    SAO_LUIS("São Luís", "MA", 752, 222),
     TERESINA("Teresina", "PI", 791, 283),
-    ARACAJU("Aracaju", "SE", 700, 440),
+    ARACAJU("Aracaju", "SE", 920, 433),
     VITORIA("Vitória", "ES", 835, 652),
-    GOIANIA("Goiânia", "GO", 500, 520),
+    GOIANIA("Goiânia", "GO", 616, 570),
     CUIABA("Cuiabá", "MT", 459, 540),
     CAMPO_GRANDE("Campo Grande", "MS", 490, 670),
     MANAUS("Manaus", "AM", 360, 230),
@@ -24,7 +24,7 @@ public enum Capital {
     RIO_BRANCO("Rio Branco", "AC", 170, 405),
     BELEM("Belém", "PA", 650, 180),
     MACAPA("Macapá", "AP", 583, 147),
-    PALMAS("Palmas", "TO", 570, 360),
+    PALMAS("Palmas", "TO", 647, 405),
     CURITIBA("Curitiba", "PR", 613, 782),
     FLORIANOPOLIS("Florianópolis", "SC", 630, 842),
     PORTO_ALEGRE("Porto Alegre", "RS", 570, 900);
@@ -41,10 +41,21 @@ public enum Capital {
         this.y = y;
     }
 
-    public String getNome() { return nome; }
-    public String getSigla() { return sigla; }
-    public int getX() { return x; }
-    public int getY() { return y; }
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 
     @Override
     public String toString() {
